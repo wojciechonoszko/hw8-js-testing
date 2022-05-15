0e1c7b376031e5f9caaecca7f19d9e0440afd4a2
Wojciech <wojciechonoszko@gmail.com>
2022-05-15 17:27:32 +0200

    update

--- moja-aplikacja/src/index.js
+++ moja-aplikacja/src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+
+const link = React.createElement(
+  "a",
+  {
+      href: "https://interia.pl",
+      target: "_blank",
+      rel: "noreferrer noopener",
+
+  },
+  "Go to interia.pl"
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// link with JSX
+
+const linkWithJsx = (
+  <a href="https://interia.pl/" target="_blank" rel="noreferrer noopener">
+  "Go to interia.pl"
+</a>
+)
+
+
+const image = React.createElement("img", {
+  src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+  alt: "Tacos With Lime",
+  width: 640,
+});
+ const title = React.createElement("h2", null, "Tacos With Lime");
+ const price = React.createElement("p", null, "Price: 10.99$");
+ const button = React.createElement("button", {type: "button"}, "Add to cart");
+
+ const product = React.createElement("div", null, image, title, price, button);
+
+ //LUB
+
+ const productWithChildrenInProps = React.createElement("div", {
+   children: [image, title, price, button],
+ });
+
+ // with JSX
+
+ const imageUrl = 
+ "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const priceJSX = 10.99;
+
+const productJSX = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {priceJSX}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+)
+
+ ReactDOM.createRoot(document.getElementById('root').render(product));
+ ReactDOM.createRoot(document.getElementById('root').render(productWithChildrenInProps));
+ ReactDOM.createRoot(document.getElementById('root').render(link));
+ ReactDOM.createRoot(document.getElementById('root').render(linkWithJsx));
+ ReactDOM.createRoot(document.getElementById('root').render(productJSX));
